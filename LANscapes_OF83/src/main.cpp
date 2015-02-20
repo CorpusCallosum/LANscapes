a125caf1b789d0d1ca3a44c3eedda5420bc93bfb
Jack Kalish <jykalish@gmail.com>
2015-02-19 20:43:44 -0500

    added ofxwatchdog

--- LANscapes_OF83/src/main.cpp
+++ LANscapes_OF83/src/main.cpp
@@ -1,9 +1,19 @@
 #include "ofApp.h"
 #include "ofMain.h"
 #include "ofAppGlutWindow.h"
+#include "ofxWatchdog.h"
+
 
 //========================================================================
 int main( ){
+    
+    // ofxWatchdog::watch(msec, reboot, override, verbose)
+    //
+    //     msec : how long does the watchdog wait, when the process hangs-up in milli seconds
+    //   reboot : automatically restart the process
+    // override : use internal signal handler (optional)
+    //  verbose : print more log information (optional)
+    ofxWatchdog::watch(3000, true, true, true);
     
 	//ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
     
